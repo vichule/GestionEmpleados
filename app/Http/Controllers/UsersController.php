77e422336f17bdfe05676d96c01ba2f441e5ae54
Javier Cabañas <vichule@hotmail.com>
2021-12-01 18:24:34 +0100

    env correcto y funcion registrar creada(falta añadir verificaciones)

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    public function registrarEmpleado(Request $req){
+
+    	$password = $req->password;
+
+    	$valido = true;
+    }
 }
